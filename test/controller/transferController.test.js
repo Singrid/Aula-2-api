198fa1f5f9334eaf8cbef6652e6c390fb1f8c18f
singrid.palmeira <singrid.palmeira@virtus.ufcg.edu.br>
2025-09-16 10:57:01 -0300

    adicionando o primeiro teste pra validar o controller

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../app');
+
+//Testes sem uso de simulador
+describe('Transfer Controller', () => {
+    describe('POST /transfer', () => {
+        it('Quando informo remetente e destinatario inexistente recebo 400', async() => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: "Pedro",
+                    to: "Luiza",
+                    amount: 100
+                });
+
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.deep.equal({ error: 'Usuário remetente ou destinatário não encontrado.' });
+            //Ou pode ser feito assim
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.');
+        });
+    });
+
+    describe("GET /trasnfers", () => {});
+});
