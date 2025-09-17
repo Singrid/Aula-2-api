8d516530e14abbf23a7fbd78450c0f3f1633e6e2
singrid.palmeira <singrid.palmeira@virtus.ufcg.edu.br>
2025-09-16 22:48:14 -0300

    Adicionando um novo teste de erro na transferencia com Mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -4,6 +4,9 @@ const { expect } = require('chai');
 
 const app = require('../../app');
 
+//Mock
+const transferService = require('../../service/transferService');
+
 //Testes sem uso de simulador
 describe('Transfer Controller', () => {
     describe('POST /transfer', () => {
@@ -20,6 +23,29 @@ describe('Transfer Controller', () => {
             expect(resposta.body).to.deep.equal({ error: 'Usuário remetente ou destinatário não encontrado.' });
             //Ou pode ser feito assim
             expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.');
+        });
+
+        it('Usando Mocks: Quando informo remetente e destinatario inexistente recebo 400', async() => {
+            //mocar apenas a função transfer do service
+            const transferServiceMock = sinon
+                .stub(transferService, 'transfer');
+            transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado.'));
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: "Pedro",
+                    to: "Luiza",
+                    amount: 1000
+                });
+
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.deep.equal({ error: 'Usuário remetente ou destinatário não encontrado.' });
+            //Ou pode ser feito assim
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado.');
+
+            //reseta o Mock
+            sinon.restore();
         });
     });
 
